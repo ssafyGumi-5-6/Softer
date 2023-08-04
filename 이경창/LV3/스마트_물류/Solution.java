943a295b5f3fe093742059aa271aa6f1846d8b9a
이경창 <lkc263@naver.com>
2023-08-04 16:46:46 +0900

    스마트 물류

---
+++ 이경창/LV3/스마트_물류/Solution.java
@@ -0,0 +1,72 @@
+package Softeer.LV3.스마트_물류;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Solution
+{
+    private static int N, K;
+    private static int answer;
+    private static char[] distribution;
+    private static boolean[] visited;
+
+    private static int leftFoundH(int index){
+        int findHIndex = -1;
+
+        for(int i = index - 1; i >= index - K && i >= 0; i--){
+            if(distribution[i] == 'H' && !visited[i]) findHIndex = i;
+        }
+
+        return findHIndex;
+    }
+
+    private static int rightFoundH(int index){
+        for(int i = index + 1; i <= index + K && i < N; i++){
+            if(distribution[i] == 'H' && !visited[i]) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        N = Integer.parseInt(tokenizer.nextToken());
+        K = Integer.parseInt(tokenizer.nextToken());
+        visited = new boolean[N];
+
+
+        distribution = reader.readLine().toCharArray();
+
+        for(int index = 0; index < distribution.length; index++){
+            // 만약 P라면 체크
+            if(distribution[index] == 'P'){
+                // System.out.println("index : " + index);
+                // (1) 왼쪽 탐색
+                int leftFoundIndex = leftFoundH(index);
+                if(leftFoundIndex == -1){
+                    int rightFoundIndex = rightFoundH(index);
+
+                    if(rightFoundIndex != -1){
+                        // System.out.println("rightFoundIndex : " + rightFoundIndex);
+                        visited[rightFoundIndex] = true;
+                        answer += 1;
+                    }
+                }else{
+                    // System.out.println("leftFoundIndex : " + leftFoundIndex);
+                    visited[leftFoundIndex] = true;
+                    answer += 1;
+                }
+
+
+            }
+        }
+
+        // System.out.println(Arrays.toString(visited));
+        System.out.println(answer);
+
+        reader.close();
+    }
+}
--- 이경창/LV3/택배_마스터_광우/Solution.java
+++ 이경창/LV3/택배_마스터_광우/Solution.java
@@ -1,4 +1,6 @@
 package Softeer.LV3.택배_마스터_광우;
 
 public class Solution {
+    // 문제 이해가 안됨
+    // 순열, k 반복문, while문을 통해 구하면 될 것 같은데 문제 이해가 안됨
 }
